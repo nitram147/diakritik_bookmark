a88a8a23c68ebf90efc633edee722a3353b6f2fe
nitram147 <nitram147@gmail.com>
2016-12-11 17:23:04 +0100

    Update source_sk.js
    Fix preklepu

--- source_sk.js
+++ source_sk.js
@@ -1,4 +1,4 @@
 var text = prompt("Prosím zadajte text", "Nejaky text bez diakritiky");
-        $.get("http://vasaweb.sk/priecinok_diakritik_api/?plugin=ano&text="+text, function(data, status){
+        $.get("http://vasweb.sk/priecinok_diakritik_api/?plugin=ano&text="+text, function(data, status){
             alert(data);
         });
